c34a6bfae063afc45d4e47e6720eb59e0bb03958
nicolo <andreiafurbeis@gmail.com>
2018-04-09 16:15:36 +0200

    classe compito fatta

--- app/src/main/java/com/example/andrearubeis/wash_up/Compito.java
+++ app/src/main/java/com/example/andrearubeis/wash_up/Compito.java
@@ -8,10 +8,55 @@ public class Compito {
 
 
     private String id;
+    private String descrizione;
+    private String stanza;
+    private String id_casa;
 
-    public Compito(){
+
+
+
+
+
+    public Compito(String id, String descrizione, String stanza, String id_casa){
+
+        this.id = id;
+        this.descrizione = descrizione;
+        this.stanza = stanza;
+        this.id_casa = id_casa;
 
     }
 
 
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getId_casa() {
+        return id_casa;
+    }
+
+    public String getStanza() {
+        return stanza;
+    }
+
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setId_casa(String id_casa) {
+        this.id_casa = id_casa;
+    }
+
+    public void setStanza(String stanza) {
+        this.stanza = stanza;
+    }
 }
