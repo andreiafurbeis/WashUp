d45bd515e58e32bcc1ccb6ffbbd7c60125ca8025
nicolo <andreiafurbeis@gmail.com>
2018-04-11 12:14:26 +0200

    implementata list view sul fragment ruoli, con creazione dinamica di pulsanti cliccabili.

---
+++ app/src/main/java/com/example/andrearubeis/wash_up/AdapterRuoli.java
@@ -0,0 +1,119 @@
+package com.example.andrearubeis.wash_up;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/**
+ * Created by nicolo on 10/04/18.
+ */
+
+public class AdapterRuoli extends ArrayAdapter<Persona> {
+
+    Context context;
+    ArrayList<Persona> data;
+    private static LayoutInflater inflater = null;
+
+    public AdapterRuoli(Context context, ArrayList<Persona> data) {
+        super(context, R.layout.item_persona , data);
+        this.context = context;
+        this.data = data;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    }
+
+
+    @Override
+    public int getCount() {
+       return data.size();
+    }
+
+    @Override
+    public Persona getItem(int position) {
+        return  data.get(position);
+    }
+
+
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+
+        View rowView = convertView;
+
+        if(rowView == null){
+            rowView = inflater.inflate(R.layout.item_persona, parent, false);
+
+        }
+
+        Button campanella = (Button) rowView.findViewById(R.id.item_persona_button_bell);
+        TextView row_inquilino = (TextView) rowView.findViewById(R.id.item_persona_text_view_nome);
+        //row_inquilino.setCompoundDrawablesRelativeWithIntrinsicBounds(getDrawable(data[position].getProfileImage()),null,null,null);
+        row_inquilino.setText(data.get(position).getNome());
+        row_inquilino.setCompoundDrawablesWithIntrinsicBounds( getDrawable(data.get(position).getProfileImage()), null, null, null);
+
+        campanella.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("AdapterRuoli","e stata cliccata la campanella");
+
+            }
+        });
+
+        rowView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("AdapterRuoli" , "e stato cliccato un bottone");
+                Intent intent = new Intent(context, RuoliInquilino.class);
+                Bundle bundle = new Bundle();
+                intent.putExtra("nome_persona",data.get(position).getNome() );
+                intent.putExtras(bundle);
+                context.startActivity(intent);
+
+                //Log.d("log2","SONO IN RUOLIFRAGMENTACTIVITY,STARTACTIVITY(intent) ");
+            }
+        });
+
+        return rowView;
+
+    }
+
+
+
+    public Drawable getDrawable(String image) {
+        Drawable image_drawable;
+        String[] image_path = image.split(" ");
+
+
+        Toast.makeText(context, "Sto mettendo l'immagine con il metodo nuovo", Toast.LENGTH_SHORT).show();
+        ImageManager manager = new ImageManager(context);
+        Bitmap image_bitmap = manager.loadImageFromStorage(image_path[0], image_path[1]);
+        //Log.d("OptionFragment" , "il path è : " + image_path[0] + "   " + image_path[1]);
+        Bitmap image_bitmap_scaled = Bitmap.createScaledBitmap(image_bitmap , ViewGroup.LayoutParams.MATCH_PARENT , context.getResources().getDisplayMetrics().heightPixels/6,true);
+        image_drawable = new BitmapDrawable(context.getResources(), image_bitmap);
+
+
+        return image_drawable;
+    }
+
+
+
+}
--- app/src/main/java/com/example/andrearubeis/wash_up/RuoliInquilino.java
+++ app/src/main/java/com/example/andrearubeis/wash_up/RuoliInquilino.java
@@ -4,5 +4,80 @@ package com.example.andrearubeis.wash_up;
  * Created by nicoloricci on 27/12/17.
  */
 
-public class RuoliInquilino {
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.example.andrearubeis.wash_up.R;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Vector;
+import java.util.concurrent.ExecutionException;
+public class RuoliInquilino extends AppCompatActivity {
+
+
+
+
+    Button title ;
+
+
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ruoli_x_inquilino);
+
+        ActionBar barra = getSupportActionBar();
+        barra.hide();
+
+
+        Intent intent = getIntent();
+
+
+        String nome_persona = intent.getStringExtra("nome_persona");
+
+        title = (Button) findViewById(R.id.ruoli_x_inquilino_title);
+        title.setText(nome_persona);
+
+
+
+    }
+
+
+
+
+
+
 }
